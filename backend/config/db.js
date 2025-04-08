fc35705abd7f85018285671e96787fd3ebfdfdbe
Deepak8218 <deepakprince917@gmail.com>
2025-04-08 22:23:33 +0530

    Initial commit for folder structure in backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose";
+// config/db.js
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,27 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./config/db.js";
+// server.js
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import cors from 'cors';
 
 dotenv.config();
-connectDB();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
+// DB Connection
+connectDB();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+// Default route
+app.get('/', (req, res) => {
+  res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
